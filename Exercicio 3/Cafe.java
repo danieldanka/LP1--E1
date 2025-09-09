445056214a1630f1c7a4e7e72b18d4c7729dc109
Daniel Natan <73396751+danieldanka@users.noreply.github.com>
2025-09-09 19:39:38 -0300

    Create Cafe.java

---
+++ Exercicio 3/Cafe.java
@@ -0,0 +1,24 @@
+public class Cafe {
+
+    private String grao;
+    private String torra;
+    private int temperatura;
+
+    public Cafe(String grao, String torra, int temperatura) {
+        this.grao = grao;
+        this.torra = torra;
+        this.temperatura = temperatura;
+    }
+
+    public void Queimar() {
+        System.out.println("O café foi queimado.");
+    }
+
+    public void Esfriar() {
+        System.out.println("O café está esfriando.");
+    }
+
+    public void Hidratar() {
+        System.out.println("O café está hidratando.");
+    }
+}
